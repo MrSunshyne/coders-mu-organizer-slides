bcd2919660797ae86469ec1e2378a73d50a9cac9
Sandeep Ramgolam <sandeep@ramgolam.com>
2025-10-18 00:28:43 +0400

    add emoji controls

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <AudienceReactions 
+        v-if="reactionsEnabled"
         :host="REACTIONS_HOST"
         :show-controls="false"
         :show-status="false"
@@ -11,8 +12,12 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { useStorage } from '@vueuse/core'
 import AudienceReactions from './components/audience/AudienceReactions.vue'
 
 const REACTIONS_HOST = 'slidev-audience.s4ndeep1203.workers.dev'
+
+// Use the same storage key as custom-nav-controls.vue
+const reactionsEnabled = useStorage('slidev-reactions-enabled', true)
 </script>
